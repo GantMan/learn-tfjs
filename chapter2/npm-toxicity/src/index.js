b1b333e0714f7fddc778b487863252cd650ba357
Gant Laborde <gantman@gmail.com>
2020-09-21 21:15:32 -0500

    add toxicity example

---
+++ chapter2/npm-toxicity/src/index.js
@@ -0,0 +1,15 @@
+import "@tensorflow/tfjs";
+import * as toxicity from "@tensorflow-models/toxicity";
+
+// minimum positive prediction confidence
+// If this isn't passed, the default is 0.85
+const threshold = 0.5;
+
+toxicity.load(threshold).then((model) => {
+  const sentences = ["You are a poopy head!", "I like turtles", "Shut up!"];
+
+  model.classify(sentences).then((predictions) => {
+    // semi-pretty-print results
+    console.log(JSON.stringify(predictions, null, 2));
+  });
+});
