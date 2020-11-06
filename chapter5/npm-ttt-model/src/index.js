a280a3e805dad431978a2a8a32948c3c88e5d54e
Gant Laborde <gantman@gmail.com>
2020-11-06 01:26:12 -0600

    example of reading from tfhub

--- chapter5/npm-ttt-model/src/index.js
+++ chapter5/npm-ttt-model/src/index.js
@@ -1,20 +1,19 @@
 import * as tf from "@tensorflow/tfjs";
 
-
 tf.ready().then(() => {
-  const modelPath = "model/ttt_model.json"
+  const modelPath = "model/ttt_model.json";
   tf.tidy(() => {
-    tf.loadLayersModel(modelPath).then(model => {
+    tf.loadLayersModel(modelPath).then((model) => {
       // Three board states
-      const emptyBoard = tf.zeros([9])
-      const betterBlockMe = tf.tensor([-1, 0, 0, 1, 1, -1, 0, 0, -1])
-      const goForTheKill = tf.tensor([1, 0, 1, 0, -1, -1, -1, 0, 1])
+      const emptyBoard = tf.zeros([9]);
+      const betterBlockMe = tf.tensor([-1, 0, 0, 1, 1, -1, 0, 0, -1]);
+      const goForTheKill = tf.tensor([1, 0, 1, 0, -1, -1, -1, 0, 1]);
 
       // Stack states into a shape [3, 9]
-      const matches = tf.stack([emptyBoard, betterBlockMe, goForTheKill])
-      const result = model.predict(matches)
+      const matches = tf.stack([emptyBoard, betterBlockMe, goForTheKill]);
+      const result = model.predict(matches);
       // Log the results
-      result.reshape([3, 3, 3]).print()
-    })
-  })
-})
+      result.reshape([3, 3, 3]).print();
+    });
+  });
+});
