017c16a87fdd8963a5e4055a348ca1f5a6d0d521
Gant Laborde <gantman@gmail.com>
2020-12-13 15:53:45 -0600

    updates

--- chapter9/node/node-naive-train/4_clean.js
+++ chapter9/node/node-naive-train/4_clean.js
@@ -1,9 +1,9 @@
-const dfd = require("danfojs-node");
-const prep = require("./3_combine");
+const dfd = require('danfojs-node')
+const prep = require('./3_combine')
 
 async function cleanData() {
   // Combine the CSV data using './3_combine.js
-  const mega = await prep.prepareData();
+  const mega = await prep.prepareData()
 
   /* CLEAN THE DATA
    * ------------------
@@ -14,34 +14,37 @@ async function cleanData() {
 
   // Remove feature columns that seem less useful
   const clean = mega.drop({
-    columns: ["Name", "PassengerId", "Ticket", "Cabin"],
-  });
+    columns: ['Name', 'PassengerId', 'Ticket', 'Cabin'],
+  })
 
   // Remove all rows that have empty spots
-  const onlyFull = clean.dropna();
-  console.log(`After mega-clean the row-count is now ${onlyFull.shape[0]}`);
+  const onlyFull = clean.dropna()
+  console.log(`After mega-clean the row-count is now ${onlyFull.shape[0]}`)
 
   // Handle embarked characters - convert to numbers
-  const encode = new dfd.LabelEncoder();
-  encode.fit(onlyFull["Embarked"]);
-  onlyFull["Embarked"] = encode.transform(onlyFull["Embarked"].values);
-  onlyFull.head().print();
+  const encode = new dfd.LabelEncoder()
+  encode.fit(onlyFull['Embarked'])
+  onlyFull['Embarked'] = encode.transform(onlyFull['Embarked'].values)
+  onlyFull.head().print()
 
-  // Handle person sex - convert to one-hot
-  const sexOneHot = dfd.get_dummies(onlyFull["Sex"]);
-  sexOneHot.head().print();
-  // Swap one column for two
-  console.log(`Before shape ${onlyFull.shape}`);
-  onlyFull.drop({ columns: ["Sex"], axis: 1, inplace: true });
-  onlyFull.addColumn({ column: "male", value: sexOneHot["male"] });
-  onlyFull.addColumn({ column: "female", value: sexOneHot["female"] });
+  // // Handle person sex - convert to one-hot
+  // const sexOneHot = dfd.get_dummies(onlyFull["Sex"]);
+  // sexOneHot.head().print();
+  // // Swap one column for two
+  // console.log(`Before shape ${onlyFull.shape}`);
+  // onlyFull.drop({ columns: ["Sex"], axis: 1, inplace: true });
+  // onlyFull.addColumn({ column: "male", value: sexOneHot["male"] });
+  // onlyFull.addColumn({ column: "female", value: sexOneHot["female"] });
+  encode.fit(onlyFull['Sex'])
+  onlyFull['Sex'] = encode.transform(onlyFull['Sex'].values)
+
   // The results
-  onlyFull.head().print();
-  return onlyFull;
+  onlyFull.head().print()
+  return onlyFull
 }
 
 // Demonstrate
-cleanData();
+cleanData()
 
 // Export for use in subsequent steps
-module.exports = { cleanData };
+module.exports = { cleanData }
--- chapter9/node/node-naive-train/6_train.js
+++ chapter9/node/node-naive-train/6_train.js
@@ -1,34 +1,34 @@
-const dfd = require("danfojs-node");
-const tf = require("@tensorflow/tfjs"); // Use if having issues with tfjs-node
-// const tf = require("@tensorflow/tfjs-node");
+const dfd = require('danfojs-node')
+// const tf = require("@tensorflow/tfjs"); // Use if having issues with tfjs-node
+const tf = require('@tensorflow/tfjs-node')
 
-const modelMaker = require("../node-shared-model/getModel");
+const modelMaker = require('../node-shared-model/getModel')
 
 async function trainModel() {
   // Get cleaned data
-  const df = await dfd.read_csv("file://../../extra/cleaned/newTrain.csv");
-  console.log("Train Size", df.shape[0]);
-  const dft = await dfd.read_csv("file://../../extra/cleaned/newTest.csv");
-  console.log("Test Size", dft.shape[0]);
+  const df = await dfd.read_csv('file://../../extra/cleaned/newTrain.csv')
+  console.log('Train Size', df.shape[0])
+  const dft = await dfd.read_csv('file://../../extra/cleaned/newTest.csv')
+  console.log('Test Size', dft.shape[0])
 
   // Split train into X/Y
-  const trainX = df.iloc({ columns: [`1:`] }).tensor;
-  const trainY = df["Survived"].tensor;
+  const trainX = df.iloc({ columns: [`1:`] }).tensor
+  const trainY = df['Survived'].tensor
 
   // Split test into X/Y
-  const testX = dft.iloc({ columns: [`1:`] }).tensor;
-  const testY = dft["Survived"].tensor;
+  const testX = dft.iloc({ columns: [`1:`] }).tensor
+  const testY = dft['Survived'].tensor
 
   // Make and train
-  const inputShape = [df.shape[1] - 1];
-  const model = modelMaker.getModel(tf, inputShape);
+  const inputShape = [df.shape[1] - 1]
+  const model = modelMaker.getModel(tf, inputShape)
   await model.fit(trainX, trainY, {
     batchSize: 32,
     epochs: 100,
     validationData: [testX, testY],
     // callbacks: modelMaker.callbacks, // use when using tfjs instead of tfjs-node
-  });
+  })
 }
 
 // Demonstrate
-trainModel();
+trainModel()
