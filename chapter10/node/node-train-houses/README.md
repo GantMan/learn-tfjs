2a8fe690f2943581dbdc9d43b936320fc38367f7
Gant Laborde <gantman@gmail.com>
2020-12-25 23:53:16 -0600

    add training data

---
+++ chapter10/node/node-train-houses/README.md
@@ -0,0 +1,3 @@
+# Data 
+
+To get the data ready for training, unzip the image zip files in the src folder.
---
+++ chapter10/node/node-train-houses/src/index.js
@@ -0,0 +1,11 @@
+import * as tf from '@tensorflow/tfjs-node'
+import * as fs from 'fs'
+import * as path from 'path'
+
+async function doTraining() {
+  console.log(tf.version)
+  
+}
+
+doTraining()
+
