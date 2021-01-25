f7afc5f77ba733a1f3263560f3326656eb5b1b8b
Gant Laborde <gantman@gmail.com>
2021-01-25 14:20:59 -0600

    getting training closer

--- chapter12/node/node-train-dice/src/index.js
+++ chapter12/node/node-train-dice/src/index.js
@@ -1,102 +1,120 @@
 import * as tf from '@tensorflow/tfjs-node'
-const fs = require('fs')
 
-const pixelShift = async (inputTensor, mutations = []) => {
-  // Add 1px white padding to height and width
-  const padded = inputTensor.pad(
-    [
-      [1, 1],
-      [1, 1],
-    ],
-    1
+const inputShape = [12, 12, 1]
+const epochs = 10
+const diceData = require('./dice_data.json').dice
+
+// Wrap in a tidy for memory
+const [stackedX, stackedY] = tf.tidy(() => {
+  // Build a stacked tensor from JSON
+  const xs = tf
+    .concat([
+      diceData['0'],
+      diceData['1'],
+      diceData['2'],
+      diceData['3'],
+      diceData['4'],
+      diceData['5'],
+      diceData['6'],
+      diceData['7'],
+      diceData['8'],
+      diceData['inverted0'],
+      diceData['inverted1'],
+      diceData['inverted2'],
+      diceData['inverted3'],
+      diceData['inverted4'],
+      diceData['inverted5'],
+      diceData['inverted6'],
+      diceData['inverted7'],
+      diceData['inverted8'],
+    ])
+    .expandDims(3)
+
+  // Now the answers to their corresponding index
+  const answers = [].concat(
+    new Array(diceData['0'].length).fill(0),
+    new Array(diceData['1'].length).fill(1),
+    new Array(diceData['2'].length).fill(2),
+    new Array(diceData['3'].length).fill(3),
+    new Array(diceData['4'].length).fill(4),
+    new Array(diceData['5'].length).fill(5),
+    new Array(diceData['6'].length).fill(6),
+    new Array(diceData['7'].length).fill(7),
+    new Array(diceData['8'].length).fill(8),
+    new Array(diceData['inverted0'].length).fill(9),
+    new Array(diceData['inverted1'].length).fill(10),
+    new Array(diceData['inverted2'].length).fill(11),
+    new Array(diceData['inverted3'].length).fill(12),
+    new Array(diceData['inverted4'].length).fill(13),
+    new Array(diceData['inverted5'].length).fill(14),
+    new Array(diceData['inverted6'].length).fill(15),
+    new Array(diceData['inverted7'].length).fill(16),
+    new Array(diceData['inverted8'].length).fill(17)
   )
-  const cutSize = inputTensor.shape
-  for (let h = 0; h < 3; h++) {
-    for (let w = 0; w < 3; w++) {
-      mutations.push(padded.slice([h, w], cutSize))
-    }
-  }
-  padded.dispose()
-  return mutations
-}
+  const ys = tf.oneHot(answers, diceData.length)
 
-// Creates combinations take any two from inputAarray (like Py itertools.combinations)
-const combos = async (tensorArray) => {
-  const startSize = tensorArray.length
-  for (let i = 0; i < startSize - 1; i++) {
-    for (let j = i + 1; j < startSize; j++) {
-      const overlay = tf.tidy(() => {
-        return tf.where(
-          tf.less(tensorArray[i], tensorArray[j]),
-          tensorArray[i],
-          tensorArray[j]
-        )
-      })
-      tensorArray.push(overlay)
-    }
+  return [xs, ys]
+})
+
+const testModel = (model, img2Test) => {
+  const img = new Image()
+  img.crossOrigin = 'anonymous'
+  img.src = img2Test
+  img.onload = async () => {
+    const imgTensor = tf.browser.fromPixels(img, 1).expandDims()
+
+    const tensorResults = model.predict(imgTensor)
+    const results = tensorResults.arraySync()
+
+    console.log(`${img2Test} returned ${results}`)
   }
 }
 
-// Remove duplicates and stack into a 4D tensor
-const consolidate = async (tensorArray) => {
-  const groupedData = tf.stack(tensorArray)
-  console.log('Grouped Data:', groupedData.shape)
-  // Needs to switch processing to CPU for `tf.unique` on Node
-  // See: https://github.com/tensorflow/tfjs/issues/4595
-  await tf.setBackend('cpu')
-  const { values, _indices } = tf.unique(groupedData)
+const trainModel = async () => {
+  const model = tf.sequential()
 
-  tf.dispose([groupedData, _indices])
-  tf.dispose(tensorArray)
-  return values
-}
+  model.add(tf.layers.flatten({ inputShape }))
 
-const runAugmentation = async (aTensor) => {
-  const mutes = await pixelShift(aTensor)
-  await combos(mutes)
-  await combos(mutes)
-  return await consolidate(mutes)
-}
-
-const flipTensor = (dit) => {
-  return tf.tidy(() => {
-    // const flip = tf.logicalNot(dit.asType("bool")).asType("float32")
-    const flip = dit.sub(1).mul(-1)
-    return flip
+  model.add(
+    tf.layers.dense({
+      units: 64,
+      inputShape: inputShape,
+      activation: 'relu',
+    })
+  )
+  model.add(
+    tf.layers.dense({
+      units: 8,
+      activation: 'relu',
+    })
+  )
+  model.add(
+    tf.layers.dense({
+      units: 9,
+      kernelInitializer: 'varianceScaling',
+      activation: 'softmax',
+    })
+  )
+  const learningRate = 0.005
+  model.compile({
+    optimizer: tf.train.adam(learningRate),
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
   })
-}
 
-const createDataObject = async () => {
-  // Tensor Array
-  const inDice = require('./dice.json').data
-  const diceData = {}
-
-  for (let idx = 0; idx < inDice.length; idx++) {
-    console.log(idx)
-    const die = inDice[idx]
-    const imgTensor = tf.tensor(die)
-    const results = await runAugmentation(imgTensor)
-    console.log('Results:', results.shape)
-    const invertedResults = flipTensor(results)
-    console.log('Inverted:', invertedResults.shape)
-
-    // Store results
-    diceData[idx] = results.arraySync()
-    diceData[`inverted${idx}`] = invertedResults.arraySync()
-
-    tf.dispose([results, imgTensor, invertedResults])
+  // Make loss callback
+  const printCallback = {
+    onEpochEnd: (epoch, log) => {
+      console.log(`${epoch + 1} of ${epochs}`, log)
+    },
   }
 
-  const jsonString = JSON.stringify(diceData)
-  fs.writeFile('dice_data.json', jsonString, (err) => {
-    if (err) throw err
-    console.log('Data written to file')
+  await model.fit(stackedX, stackedY, {
+    epochs,
+    shuffle: true,
+    batchSize: 32,
+    callbacks: printCallback,
   })
-  console.log('Memory', tf.memory().numTensors)
-}
 
-try {
-  createDataObject()
-} catch (e) {
-  console.error('ERROR', e)
+  console.log('Done')
 }
