f1ff98d8e5c1dd061efcfa570fbdcb52ce405f74
Gant Laborde <gantman@gmail.com>
2021-01-24 18:21:14 -0600

    add code to start

--- chapter12/node/node-dice-data/src/index.js
+++ chapter12/node/node-dice-data/src/index.js
@@ -1,4 +1,4 @@
-import * as tf from "@tensorflow/tfjs-node";
+import * as tf from '@tensorflow/tfjs-node'
 
 const pixelShift = async (inputTensor, mutations = []) => {
   // Add 1px white padding to height and width
@@ -6,50 +6,51 @@ const pixelShift = async (inputTensor, mutations = []) => {
     [
       [1, 1],
       [1, 1],
-      [0, 0]
+      [0, 0],
     ],
-    255
-  );
-  const cutSize = inputTensor.shape;
+    1
+  )
+  const cutSize = inputTensor.shape
   for (let h = 0; h < 3; h++) {
     for (let w = 0; w < 3; w++) {
-      mutations.push(padded.slice([h, w, 0], cutSize));
+      mutations.push(padded.slice([h, w, 0], cutSize))
     }
   }
-  padded.dispose();
-  return mutations;
-};
+  padded.dispose()
+  return mutations
+}
 
 // Creates combinations take any two from inputAarray (like Py itertools.combinations)
 const combos = async (tensorArray) => {
-  const arrCopy = [...tensorArray];
-  const startSize = arrCopy.length;
+  const arrCopy = [...tensorArray]
+  const startSize = arrCopy.length
   for (let i = 0; i < startSize - 1; i++) {
     for (let j = i + 1; j < startSize; j++) {
       const overlay = tf.where(
         tf.less(arrCopy[i], arrCopy[j]),
         arrCopy[i],
         arrCopy[j]
-      );
-      tensorArray.push(overlay);
+      )
+      tensorArray.push(overlay)
     }
   }
-};
+}
 
 // Remove duplicates and stack into a 4D tensor
 const consolidate = async (tensorArray) => {
-  const groupedData = tf.stack(tensorArray);
-  const { values } = tf.unique(groupedData);
-  groupedData.dispose();
-  return values;
-};
+  const groupedData = tf.stack(tensorArray)
+  const { values } = tf.unique(groupedData)
+  groupedData.dispose()
+  return values
+}
 
 const runData = async (aTensor) => {
-  const mutes = await pixelShift(aTensor);
-  await combos(mutes);
-  await combos(mutes);
-  return await consolidate(mutes);
-};
+  const mutes = await pixelShift(aTensor)
+  await combos(mutes)
+  await combos(mutes)
+  return await consolidate(mutes)
+  // return mutes
+}
 
 const flipTensor = (dit) => {
   // const flip = tf.logicalNot(dit.asType("bool")).asType("float32")
@@ -59,15 +60,19 @@ const flipTensor = (dit) => {
 
 const doStuff = async () => {
   // Tensor Array
-  const inTensor = require("./inTensor.json").data;
+  const inTensor = require('./dice.json').data
   // some image here
-  const imgTensor = tf.tensor(inTensor[0]);
-  const results = await runData(imgTensor);
-  const augResults = results.reshape([results.shape[0], 12, 12]).arraySync();
+  const imgTensor = tf.tensor(inTensor[0], [12, 12, 1])
+  const results = await runData(imgTensor)
+  const augResults = results.reshape([results.shape[0], 12, 12]).arraySync()
 
   console.log(augResults.shape)
   tf.dispose([augResults, results, imgTensor])
   console.log(tf.memory().numTensors)
-};
+}
 
-doStuff();
+try {
+  doStuff()
+} catch (e) {
+  console.error('ERROR', e)
+}
